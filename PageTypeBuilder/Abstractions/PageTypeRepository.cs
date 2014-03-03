d83db4d84c904745985c63977ce1cfd49ce75914
Gatis Bergšpics <gatisb@gmail.com>
2014-03-03 11:24:57 +0200

    update for cms7

--- PageTypeBuilder/Abstractions/PageTypeRepository.cs
+++ PageTypeBuilder/Abstractions/PageTypeRepository.cs
@@ -6,9 +6,16 @@ namespace PageTypeBuilder.Abstractions
 {
     public class PageTypeRepository : IPageTypeRepository
     {
+        //private EPiServer.DataAbstraction.IContentTypeRepository _contentTypeRepository;
+        //public PageTypeRepository()
+        //{
+        //    _contentTypeRepository = EPiServer.ServiceLocation.ServiceLocator.Current.GetInstance<IContentTypeRepository>();
+        //}
+
         public virtual IPageType Load(string name)
         {
             var pageType = PageType.Load(name);
+            //var pageType = (PageType)_contentTypeRepository.Load(name).CreateWritableClone();
             if (pageType == null)
                 return null;
             return new WrappedPageType(pageType);
@@ -17,6 +24,7 @@ namespace PageTypeBuilder.Abstractions
         public virtual IPageType Load(System.Guid guid)
         {
             var pageType = PageType.Load(guid);
+            //var pageType = (PageType)_contentTypeRepository.Load(guid).CreateWritableClone();
             if (pageType == null)
                 return null;
             return new WrappedPageType(pageType);
@@ -25,6 +33,7 @@ namespace PageTypeBuilder.Abstractions
         public virtual IPageType Load(int id)
         {
             var pageType = PageType.Load(id);
+            //var pageType = (PageType)_contentTypeRepository.Load(id).CreateWritableClone();
             if (pageType == null)
                 return null;
             return new WrappedPageType(pageType);
@@ -33,6 +42,7 @@ namespace PageTypeBuilder.Abstractions
         public virtual IEnumerable<IPageType> List()
         {
             return PageType.List().Select(pageType => new WrappedPageType(pageType)).Cast<IPageType>();
+            //return _contentTypeRepository.List().Select(pageType => new WrappedPageType((PageType)pageType)).Cast<IPageType>();
         }
 
         public virtual void Save(IPageType pageTypeToSave)
